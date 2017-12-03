17d4214ab5d5dac2846388851b7a32a2afe47eba
Peter Ericson <pdericson@pdericson.com>
2017-12-03 16:36:59 +0800

    Update config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :dashboard, DashboardWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
-  version: Application.spec(:dashboard, :vsn)
+  version: Application.spec(:dashboard, :vsn),
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
